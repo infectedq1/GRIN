989c93d980ed6fce36af4492f6996fb0c5c96e32
Valentin Schukin <hekpoxuoypr@gmail.com>
2018-06-11 23:02:08 +0700

    feat(GrinWindow): add graphics drawing

--- src/main/java/com/nstu/grin/view/GrinWindow.java
+++ src/main/java/com/nstu/grin/view/GrinWindow.java
@@ -1,10 +1,15 @@
 package com.nstu.grin.view;
 
+import com.nstu.grin.pojo.GraphLine;
+import com.nstu.grin.pojo.Graphic;
+import com.nstu.grin.utils.Utils;
 import com.sun.xml.internal.ws.message.DOMHeader;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+
+import static java.lang.Math.abs;
 
 public class GrinWindow extends JPanel {
     private int width;
@@ -15,6 +20,9 @@ public class GrinWindow extends JPanel {
     private int maxX;
     private int maxY;
     private int cellSegmLength = 4;
+
+    private int cellX;
+    private int cellY;
 
     private Color axisColor = new Color(0xFFFFFFFF);
     private Color backgroundColor = new Color(0xFF444444);
@@ -34,17 +42,35 @@ public class GrinWindow extends JPanel {
         maxY = 10;
         maxX = 10;
 
+        cellX = width / (maxX * 2);
+        cellY = height / (maxY * 2);
+
         setPreferredSize(new Dimension(width, height));
         initBuffer();
         repaint();
     }
 
+    public void drawGraphic(Graphic graphic) {
+        Utils.safeAction(graphic, g -> {
+            int currentColor = getRandomColor();
+            g.getGraphLines().stream().map(GraphLine::getPoints).forEach(
+                    points -> points.forEach(
+                            point -> buffer.setRGB(toPixelX(point.getX()), toPixelY(point.getY()), currentColor)
+                    )
+            );
+        }, () -> {});
+        repaint();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(buffer, 0, 0, null);
+    }
+
     private void initBuffer(){
         buffer = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
 
-        int cellX = width / (maxX * 2);
-        int cellY = height / (maxY * 2);
-        int color;
         Graphics g = buffer.createGraphics();
         g.setColor(backgroundColor);
         g.fillRect(0, 0, width - 1, height - 1);
@@ -66,9 +92,23 @@ public class GrinWindow extends JPanel {
         }
     }
 
-    @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        g.drawImage(buffer, 0, 0, null);
+
+
+    private int toPixelX(double val){
+        int pixel = (int)((double)cellX / 1000 * ((abs(val)) * 1000));
+        return val >= 0 ? pixel + centerX : centerX - pixel;
+    }
+
+    private int toPixelY(double val){
+        int pixel = (int)((double)cellY / 1000 * ((abs(val)) * 1000));
+        return val <= 0 ? pixel + centerY : centerY - pixel;
+    }
+
+    private int getRandomColor(){
+        return new Color(
+                (int)(Math.random() * 256),
+                (int)(Math.random() * 256),
+                (int)(Math.random() * 256)
+        ).getRGB();
     }
 }
